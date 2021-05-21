a5830738e934a885cb551eae3723d7c306236399
LuK1337 <priv.luk@gmail.com>
2021-05-21 15:32:47 +0200

    liber: Set a default Settings.Global.DEVICE_NAME
    Change-Id: I310c1ffc581b72653783d1093d9e7ebde544bb16

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
     ApertureOverlayLiber \
     CarrierConfigLiber \
     FrameworksResLiber \
+    SettingsProviderResLiber \
     SettingsResLiber \
     SystemUIResLiber
 
