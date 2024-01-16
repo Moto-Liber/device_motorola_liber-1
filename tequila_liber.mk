10140b7cdf8e7ba13a8d597e4543d4f9becdd4b1
ViShal69x <tk5762996@gmail.com>
2024-01-16 12:47:14 +0530

    liber: Welcome tequilaOS

--- tequila_liber.mk
+++ tequila_liber.mk
@@ -21,22 +21,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from liber device
 $(call inherit-product, device/motorola/liber/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common tequilaOS stuff.
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
+
+# tequilaOS
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_liber
+PRODUCT_NAME := tequila_liber
 PRODUCT_DEVICE := liber
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := Motorola One Fusion+
+PRODUCT_MODEL := motorola one fusion+
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2340
-TARGET_SCREEN_WIDTH := 1080
 
 # Build info
 BUILD_FINGERPRINT := "motorola/liber_retail/liber:11/RPIS31.Q2-42-25-1/19a8e:user/release-keys"
